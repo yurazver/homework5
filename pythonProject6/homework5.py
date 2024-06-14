343aa77710fb14daf9e61530ecbbe94904d4f107
yurazver <yurazver0@gmail.com>
2024-06-14 14:40:45 +0300

    Add files via upload

---
+++ pythonProject6/homework5.py
@@ -0,0 +1,9 @@
+#immutable_var = (1 ,True ,"apple")
+#print(immutable_var)
+#immutable_var[2] = "peach"
+#print(immutable_var)
+# нельзя изменить объект, потому что нет изменяемых объектов
+
+mutable_list =(40, [False ], "banana")
+mutable_list[1][0] = True
+print(mutable_list)
